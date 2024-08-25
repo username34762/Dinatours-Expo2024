e3afc5a8163e384d5edc10649088f58964ad026d
NayelixFab <141381204+rocs2025@users.noreply.github.com>
2024-08-24 18:03:13 -0600

    changes

--- app/api/place/[placeId]/form/route.ts
+++ app/api/place/[placeId]/form/route.ts
@@ -2,27 +2,29 @@ import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-export async function PATCH(req:Request, {params}: {params:{placeId:string}}) {
+export async function PATCH(req: Request, { params }: { params: { placeId: string } }) {
     try {
-        const {userId} = auth();
-        const {placeId} = params
-        const values = await req.json()
-        if(!userId){
-            return new NextResponse("Unauthorized")
+        const { userId } = auth();
+        const placeId = params.placeId;  // Usar la variable directamente sin redeclararla
+        const values = await req.json();
+        
+        if (!userId) {
+            return new NextResponse("Unauthorized", { status: 401 });
         }  
+
         const place = await db.place.update({
             where: {
                 id: placeId,
                 userId,
             },
-            data:{
-                ... values,
-            }
+            data: {
+                ...values,
+            },
         });
+
         return NextResponse.json(place);
     } catch (error) {
-        console.log("[PLACE FORM ID]", error)
-        return new NextResponse("Internal Error")
+        console.log("[PLACE FORM ID]", error);
+        return new NextResponse("Internal Error", { status: 500 });
     }
-    
 }
